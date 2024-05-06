fcb9b776fd54daed6e7ecd35fcd9eea6b9428266
Bokang Modise <bokangmodisebkm@gmail.com>
2024-05-06 20:32:29 +0200

    Added new Components

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../Components/HomePage.css'
+
+export default function HomePage() {
+  return (
+    <div>
+        <div className='home-center'>
+            <div className='movies-center' >
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+                <div className='movie-card'></div>
+            </div>
+        </div>
+    </div>
+  )
+}
