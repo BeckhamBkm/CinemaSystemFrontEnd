6ca4f77ed9a1211b9fcac2266da30f2266b3459a
Bokang Modise <bokangmodisebkm@gmail.com>
2024-05-06 21:12:43 +0200

    Added a movie source file

---
+++ src/Data.jsx
@@ -0,0 +1,12 @@
+export const movies = [
+    {
+        "id":0,
+        "Title":"",
+        "Genre":"",
+        "Description":"",
+        "PicUrl":"",
+        "Duration":"",
+        "Rating":""
+
+    }
+]
